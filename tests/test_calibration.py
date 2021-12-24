71126462c3fc69b1b2c9c8150e20f699c5147f6a
Kun Lin <kun.lin.596@gmail.com>
2021-12-24 13:51:34 +0900

    Stereo: implement essential matrix decomposition and tests
    Also adopt monogram notation style from Drake.

--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -2,9 +2,7 @@
 
 import os
 import cv2
-import numpy as np
 from pathlib import Path
-from mvg import features
 
 from mvg.calibration import (
     IntrinsicsCalibration,
@@ -12,7 +10,8 @@ from mvg.calibration import (
     get_chessboard_object_points,
     compute_reprejection_error,
 )
-from mvg.fundamental import Fundamental
+from mvg.camera import CameraMatrix
+from pytest import fixture
 
 
 def _detect_corners(path: Path):
@@ -35,6 +34,7 @@ def _test_calibration_data_set(path: Path, rms_threshold: float):
     camera_matrix, radial_distortion_model, debuginfo = IntrinsicsCalibration.calibrate(
         all_image_points, object_points, debug=True
     )
+    print(camera_matrix)
 
     all_extrinsics = debuginfo["all_extrinsics"]
     for i, image_points in enumerate(all_image_points):
@@ -56,63 +56,23 @@ def test_intrinsics_calibration_rms(data_root_path, intrinsics_rms_threshold):
     _test_calibration_data_set(data_root_path / "calibration/right", intrinsics_rms_threshold)
 
 
-def test_fundamental_matrix(data_root_path, fundamental_rms_threshold):
-    print("Computing feature points on left and right images...")
-    caliration_root_path = Path(data_root_path) / "calibration"
-    caliration_left_path = caliration_root_path / "left"
-    caliration_right_path = caliration_root_path / "right"
+@fixture()
+def left_camera_matrix():
+    return CameraMatrix(
+        fx=535.35887264164,
+        fy=535.6467965086524,
+        cx=342.63131029730295,
+        cy=233.77551538388735,
+        s=0.5369513302298261,
+    )
 
-    all_image_points_l = []
-    all_image_points_r = []
-    all_images_left = []
-    all_images_right = []
-    for file1, file2 in zip(
-        sorted(os.listdir(caliration_left_path)), sorted(os.listdir(caliration_right_path))
-    ):
-        if not file1.endswith('.jpg') or not file2.endswith('.jpg'):
-            continue
-        image_left = cv2.imread(str(caliration_left_path / file1))
-        image_right = cv2.imread(str(caliration_right_path / file2))
-        all_images_left.append(image_left)
-        all_images_right.append(image_right)
-        keypoints1, descriptors1 = features.SIFT.detect(image_left)
-        keypoints2, descriptors2 = features.SIFT.detect(image_right)
-        matches = features.Matcher.match(descriptors1=descriptors1, descriptors2=descriptors2)
-        points1 = []
-        points2 = []
-        good_matches = []
-        for m, n in matches:
-            if m.distance < 0.4 * n.distance:
-                good_matches.append(m)
-                points1.append(keypoints1[m.queryIdx].pt)
-                points2.append(keypoints2[m.trainIdx].pt)
-        assert len(points1) > 8 and len(points2) > 8 and len(points1) == len(points2)
-        all_image_points_l.append(np.asarray(points1))
-        all_image_points_r.append(np.asarray(points2))
 
-    all_image_points_l = all_image_points_l
-    all_image_points_r = all_image_points_r
-
-    print("\nComputing fundamental matrix and use OpenCV F for performance reference.")
-    for _, (image_points_l, image_points_r) in enumerate(
-        zip(all_image_points_l, all_image_points_r)
-    ):
-        F = Fundamental.compute(x=image_points_l, x2=image_points_r)
-        rms = Fundamental.compute_geometric_rms(F=F, x=image_points_l, x2=image_points_r)
-        F2 = cv2.findFundamentalMat(image_points_l, image_points_r)[0]
-        rms2 = Fundamental.compute_geometric_rms(F=F2, x=image_points_l, x2=image_points_r)
-
-        # samples = list(set(np.random.randint(0, len(image_points_l), 10)))
-        # Fundamental.plot_epipolar_lines(
-        #     all_images_left[i],
-        #     all_images_right[i],
-        #     image_points_l[samples],
-        #     image_points_r[samples],
-        #     F,
-        # )
-
-        print(
-            f"rms={rms:7.3f}, opencv_rms={rms2:7.3f}, {'Won' if rms < rms2 else 'Lost':5s}, "
-            f"F-norm={np.linalg.norm((F - F2)):7.3f}"
-        )
-        assert rms < fundamental_rms_threshold  # in pixel
+@fixture()
+def right_camera_matrix():
+    return CameraMatrix(
+        fx=538.7227487607054,
+        fy=538.3156703886763,
+        cx=327.26717754281043,
+        cy=248.63185822830692,
+        s=0.10515291690756252,
+    )
