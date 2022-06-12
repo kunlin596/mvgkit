9aad8ef376ef99560ea3103d4d24bbeec9c96ef8
Kun Lin <kun.lin.596@gmail.com>
2022-06-12 22:10:45 +0900

    Stereo: implement triangulation based on optimal geometric error

--- tools/run_test.py
+++ tools/run_test.py
@@ -7,6 +7,7 @@ import subprocess
 from pathlib import Path
 
 from loguru import logger
+from numpy import clip
 
 
 def _ensure_wd():
@@ -17,25 +18,9 @@ def _ensure_wd():
     ), "This script should be executed in repo root path as `./tools/run_test.py`!"
 
 
-def _main(build_dir, types):
+def main():
     _ensure_wd()
 
-    if "all" in types or "py" in types:
-        logger.info("--- MVGKIT: Running Pytest Tests ---")
-        subprocess.call(
-            f"python3 -m pytest {Path(os.getcwd()).absolute() / 'src/tests/python'}".split(" ")
-        )
-
-    if "all" in types or "cpp" in types:
-        logger.info("--- MVGKIT: Running CMake Tests ---")
-        os.environ["MVGKIT_TEST_DATA_DIR"] = str(
-            Path(os.getcwd()).absolute() / "src" / "tests" / "data"
-        )
-        os.chdir(build_dir)
-        subprocess.call(f"ctest -j{os.cpu_count()}".split(" "))
-
-
-if __name__ == "__main__":
     parser = argparse.ArgumentParser(__doc__)
     parser.add_argument("--build-dir", type=str, default="./build")
     parser.add_argument(
@@ -45,5 +30,33 @@ if __name__ == "__main__":
         choices=["cpp", "py", "all"],
         help="tests to be executed",
     )
+    parser.add_argument("--parallel", "-p", type=int, default=1, help="run tests in parallel")
+    parser.add_argument("-k", type=str)
+    parser.add_argument("-s", action="store_true")
+
     options = parser.parse_args()
-    _main(options.build_dir, options.types)
+    if options.parallel == -1:
+        parallel = os.cpu_count()
+    else:
+        parallel = clip(options.parallel, 1, os.cpu_count())
+
+    if "all" in options.types or "py" in options.types:
+        logger.info("--- MVGKIT: Running Pytest Tests ---")
+        test_path = Path(os.getcwd()).absolute() / "src/tests/python"
+        command = f"python3 -m pytest {test_path} -v"
+        command += " -s" if options.s else ""
+        command += f" -n {parallel}" if parallel != 1 else ""
+        command += f" -k {options.k}" if options.k else ""
+        subprocess.call(command.split(" "))
+
+    if "all" in options.types or "cpp" in options.types:
+        logger.info("--- MVGKIT: Running CMake Tests ---")
+        os.environ["MVGKIT_TEST_DATA_DIR"] = str(
+            Path(os.getcwd()).absolute() / "src" / "tests" / "data"
+        )
+        os.chdir(options.build_dir)
+        subprocess.call(f"ctest -j{parallel}".split(" "))
+
+
+if __name__ == "__main__":
+    main()
